0f74d2d8e58726369184755a56cd32904068330c
minh-bao <mbnguyen@hotmail.fr>
2023-05-24 17:54:36 +0200

    feat(factory): ajout des factories pour le model product et review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,15 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $this->faker->addProvider(new \Smknstd\FakerPicsumImages\FakerPicsumImagesProvider($this->faker));
         return [
-            //
+            'name'             => $this->faker->boolean(66) ? $this->faker->words(3, true) : $this->faker->word(),
+            'description'      => $this->faker->paragraphs(4, true),
+            'price'            => $this->faker->randomFloat(2, 15.50, 2556.25),
+            'product_img_path' => $this->faker->imageUrl($width = 800, $height = 600),
+            'image_alt'        => $this->faker->sentence(5),
+            'quantity'         => $this->faker->boolean(80) ? $this->faker->numberBetween(15, 2500) : 0,
+            'user_id'          => $this->faker->randomElement(User::pluck('id')->all())
         ];
     }
 }
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,16 @@ class ReviewFactory extends Factory
      */
     public function definition(): array
     {
+        $user = $this->faker->boolean(50)? User::factory()->create() : null;
         return [
-            //
+            'review'     => $this->faker->paragraph,
+            'rating'     => $this->faker->numberBetween(0, 5),
+            'alias'      => isset($user) ?  $user->name : "user_" . $this->faker->word,
+            'img_path'   => null,
+            'user_id'    => isset($user) ? $user->id :  $this->faker->randomElement(User::pluck('id')->all()),
+            'product_id' => $this->faker->randomElement(Product::pluck('id')->all())
         ];
     }
+
+
 }
