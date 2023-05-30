980ebc3cccdfcf92e7bf066f68d7874554b8584c
minh-bao <mbnguyen@hotmail.fr>
2023-05-30 05:02:58 +0200

    feat(controler): add methods to store a new review and upload image

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,64 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\Review;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\StoreReviewRequest;
 use App\Http\Requests\UpdateReviewRequest;
 
 class ReviewController extends Controller
 {
+   
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store a newly created review
      */
     public function store(StoreReviewRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $review = Review::create($validated);
+        $this->uploadImg($review,$validated, $request);
+
+        return to_route('product.show', $request->product_id)->with('Message', 'Review and image upload successfully');
     }
 
     /**
-     * Display the specified resource.
+     * Upload file img to the designated folder
+     *
+     * @param Review $review
+     * @param array $validated
+     * @param StoreReviewRequest $request
+     * @return void
      */
-    public function show(Review $review)
+    private function uploadImg(Review $review, array $validated, StoreReviewRequest $request): void
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Review $review)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateReviewRequest $request, Review $review)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Review $review)
-    {
-        //
+        Storage::disk("public_upload")->delete("image/review/".$review->image);
+        $filename = now()->format('Y-m-d')."_" .$validated['image_path']->getClientOriginalName();
+        $request->image_path->move(public_path("image/review"),$filename);
+        $review->update(['image_path' => url('/'). '/image/review/'. $filename]);
     }
 }
