92345e38bfacd84011c71e9fcfdc186def7197b0
minh-bao <mbnguyen@hotmail.fr>
2023-05-30 05:02:08 +0200

    feat(Coontroller): add methode to index and detail pages of the products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,65 +2,213 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use Inertia\Response;
+use nadar\quill\Lexer;
 use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the product.
      */
     public function index()
     {
-        //
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'products' => $this->getProductlistData()->take(20),
+        ]);   
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Retrieve and add some info in nested arrays 
+     * 
+     * @template TMapValue   
+     * @template TKey  *
+     * @return Collection
      */
-    public function create()
+    private function getProductlistData(): Collection
     {
-        //
+        $products = Product::all();
+        return $products->map(function ( $product) {
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'price' => $product->price,
+                'rating' => ceil($product->reviews->avg('rating')),
+                'reviewCount' => $product->reviews->count(),
+                'imageSrc' => $product->product_img_path,
+                'imageAlt' => $product->image_alt,
+            ];
+        });
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Show product page
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return Response
      */
-    public function store(StoreProductRequest $request)
+    public function show(Product $product, Request $request): Response
     {
-        //
+        return Inertia::render('Products/Detail', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'product' => $this->getProductInfo($product),
+            'reviews' => $this->getRatingsData($product),
+            'featured'  => $this->getProductReviewsSorted($product, $request),
+            'user'  => Auth::check() ? Auth::user() : null,
+            'review_create_url' => route('review.create', $product->id),
+        ]); 
     }
 
     /**
-     * Display the specified resource.
+     * Retrieve ratings datas for the product 
+     *
+     * @param Product $product
+     * @return array
      */
-    public function show(Product $product)
+    private function getRatingsData(Product $product): array
     {
-        //
+        return [
+            'average'   => ceil($product->reviews->avg('rating')),
+            'totalCount'=> $product->reviews->count(),
+            'countRating_1' => $product->reviews()->rates(1),
+            'countRating_2' => $product->reviews()->rates(2),
+            'countRating_3' => $product->reviews()->rates(3),
+            'countRating_4' => $product->reviews()->rates(4),
+            'countRating_5' => $product->reviews()->rates(5),
+        ];
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Retrieve product infos
+     *
+     * @param Product $product
+     * @return array
      */
-    public function edit(Product $product)
+    private function getProductInfo(Product $product) : array
     {
-        //
+        return [
+            'id' => $product->id,
+            'name' => $product->name,
+            'description' => $product->description,
+            'product_img_path' => $product->product_img_path,
+            'price' => $product->price,
+            'in_stock' => $product->quantity > 0 ? true : false,
+            'user_id' => $product->user_id,
+            'created_at' => $product->created_at,            
+        ];
+    }
+
+
+    /**
+     * Retrieve all reviews from the product sorted and filtered
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return Collection
+     */
+    public function getProductReviewsSorted(Product $product,Request $request ): Collection
+    {       
+        if( $request->sortDate){
+            $reviews = $this->sortReviewsByDate($product, $request);
+        } else if( $request->sortRate) {
+            $reviews = $this->sortReviewsByRate($product, $request);
+        }else if($request->rate){
+            $reviews = $this->filterReviewsByRate($product, $request);
+        }else{
+            $reviews = $this->getReviews($product, $request);
+        }  
+
+
+
+        return $reviews->map(function ( $review) {
+            $lexer = Str::isJson($review->review) ? new lexer($review->review) : null;
+            return [
+                'id' => $review->id,
+                'rating' => $review->rating,
+                'content' => isset($lexer) ? $lexer->render() : $review->review,
+                'image_path' => $review->image_path ?? null,
+                'author' => $this->getAuthorReview($review),
+                'avatarSrc' => $review->user_id ? $review->user->profile_photo_path : url('app_img/avatar.svg'),
+                'date'  => $review->created_at->format('Y-m-d h:i:s'),
+            ];
+        });
     }
 
     /**
-     * Update the specified resource in storage.
+     * Retrieve review's author name 
+     *
+     * @param [type] $review
+     * @return string
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    private function getAuthorReview($review): string
     {
-        //
+        if($review->user && empty($review->alias)){
+            return $review->user->name;
+        }else if( $review->alias){
+            return $review->alias;
+        }
+        return "User : Anonymous";
+    }
+
+     /**
+     * filter reviews By date
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return LengthAwarePaginator  
+     */
+    private function filterReviewsByRate(Product $product, Request $request): LengthAwarePaginator  
+    {
+        return  $product->reviews()->where('rating',$request->rate)->orderBy('created_at', 'DESC')->paginate(10);
+    }
+
+
+    /**
+     * Sort reviews By date
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return LengthAwarePaginator  
+     */
+    private function sortReviewsByDate(Product $product, Request $request): LengthAwarePaginator  
+    {
+        return  $product->reviews()->orderBy('created_at',$request->sortDate)->paginate(10);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Sort reviews By Rate
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return LengthAwarePaginator  
      */
-    public function destroy(Product $product)
+    private function sortReviewsByRate(Product $product, Request $request): LengthAwarePaginator  
     {
-        //
+        return  $product->reviews()->orderBy('rating',$request->sortRate)->paginate(10);
+    }
+
+    /**
+     * Get the reviews of the product
+     *
+     * @param Product $product
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
+    private function getReviews(Product $product, Request $request): LengthAwarePaginator
+    {
+        return  $product->reviews()->orderBy('created_at', 'desc')->paginate(10);
     }
 }
